666095558050482414f65b7342c7cb99865d5798
Avdhez <118711523+avdhez@users.noreply.github.com>
2024-12-14 08:10:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -63,20 +63,3 @@ document.addEventListener('click', function (event) {
 });
 
 
-
-
-function openInBrowser(url) {
-    if (window.navigator && window.navigator.userAgent) {
-        const userAgent = window.navigator.userAgent.toLowerCase();
-        if (userAgent.includes('android')) {
-            // Open with Android's intent for Chrome
-            window.open(url, '_system');
-        } else {
-            // Default behavior for other platforms
-            window.open(url, '_blank');
-        }
-    } else {
-        // Fallback
-        window.open(url, '_blank');
-    }
-}
